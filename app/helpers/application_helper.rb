f7792f5ce9c78ed61da3f7944175ea0ce1b1ae76
William C. Riggins III <abyss@abyssknight.com>
2010-09-18 23:29:49 -0400

    Excercises chapter 5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,9 @@ module ApplicationHelper
       "#{base_title} | #{@title}"
     end
   end
+  
+  # Show the site logo image
+  def logo
+    logo = image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
 end
